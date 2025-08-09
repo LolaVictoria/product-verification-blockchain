934b1b04e5e795f4b765662ef761473140db6336
Lola Victoria <damilolaoniyide11@gmail.com>
2025-08-09 01:29:47 +0100

    updated smart contract

--- contracts/ProductRegistry.sol
+++ contracts/ProductRegistry.sol
@@ -7,12 +7,21 @@ contract ProductRegistry {
         string serialNumber;
         address manufacturer;
         bool isRegistered;
+        uint256 registeredAt;
     }
 
-    mapping(string => Product) public products; // serialNumber => Product
+    // Mapping: serialNumber => Product
+    mapping(string => Product) public products;
 
-    event ProductRegistered(string serialNumber, string name, address manufacturer);
+    // Events
+    event ProductRegistered(
+        string serialNumber,
+        string name,
+        address manufacturer,
+        uint256 registeredAt
+    );
 
+    // Register a product
     function registerProduct(string memory _name, string memory _serialNumber) public {
         require(!products[_serialNumber].isRegistered, "Product already registered");
 
@@ -20,18 +29,40 @@ contract ProductRegistry {
             name: _name,
             serialNumber: _serialNumber,
             manufacturer: msg.sender,
-            isRegistered: true
+            isRegistered: true,
+            registeredAt: block.timestamp
         });
 
-        emit ProductRegistered(_serialNumber, _name, msg.sender);
+        emit ProductRegistered(_serialNumber, _name, msg.sender, block.timestamp);
     }
 
-    function verifyProduct(string memory _serialNumber) public view returns (
-        string memory name,
-        address manufacturer,
-        bool isRegistered
-    ) {
+    // Verify product details
+    function verifyProduct(string memory _serialNumber)
+        public
+        view
+        returns (
+            string memory name,
+            address manufacturer,
+            bool isRegistered
+        )
+    {
         Product memory product = products[_serialNumber];
         return (product.name, product.manufacturer, product.isRegistered);
+    }
+
+    // Get full product details including timestamp
+    function getProduct(string memory _serialNumber)
+        public
+        view
+        returns (
+            string memory name,
+            address manufacturer,
+            bool isRegistered,
+            uint256 registeredAt
+        )
+    {
+        Product memory product = products[_serialNumber];
+        require(product.isRegistered, "Product not found");
+        return (product.name, product.manufacturer, product.isRegistered, product.registeredAt);
     }
 }
